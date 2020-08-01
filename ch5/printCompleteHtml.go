025e2a34355476da734ca5dc2624b3007b77a72e
BookYao <workfile_BookYao@163.com>
2020-08-01 19:54:02 +0800

    Add The Go Programming Language Pracetise Title 5.7

--- ch5/printCompleteHtml.go
+++ ch5/printCompleteHtml.go
@@ -1,6 +1,9 @@
 /**
  * @Author: BookYao
- * @Description:
+ * @Description: 练习 5.7： 完善startElement和endElement函数，使其成为通用的HTML输出器。要求：输出
+注释结点，文本结点以及每个元素的属性（< a href='...'>） 。使用简略格式输出没有孩子结点
+的元素（即用 <img/> 代替 <img></img> ） 。编写测试，验证程序输出的格式正确。（详见11
+章）
  * @File:  printCompleteHtml.
  * @Version: 1.0.0
  * @Date: 2020/8/1 17:21
