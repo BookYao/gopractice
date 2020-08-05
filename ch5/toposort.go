651ea47fde95affa2b54f58e52fe569f02d64e3c
BookYao <workfile_BookYao@163.com>
2020-08-05 23:57:39 +0800

    1.  add practice title 5.10

--- ch5/toposort.go
+++ ch5/toposort.go
@@ -1,6 +1,8 @@
 /**
  * @Author: BookYao
  * @Description: anonymous function test. toposort
+  练习5.10： 重写topoSort函数，用map代替切片并移除对key的排序代码。验证结果的正确性
+（结果不唯一）    函数 toposort2()
  * @File:  toposort
  * @Version: 1.0.0
  * @Date: 2020/8/3 23:25
@@ -61,10 +63,51 @@ func toposort(m map[string][]string) [] string {
 	return order
 }
 
+func toposort2(m map[string][]string) map[int]string {
+	var order = make(map[int]string)
+	var index int = 1
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order[index] = item
+				index++
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	for key:= range keys {
+		fmt.Printf("keys[%d]=%s\n", key, keys[key])
+	}
+	fmt.Println("========================")
+
+	visitAll(keys)
+
+	return order
+}
+
 func main() {
+	// slice 方式
+	/*
 	order := toposort(prereqs)
 	for key:= range order {
 		fmt.Printf("order[%d]=%s\n", key, order[key])
+	}
+	*/
+
+	// map 方式
+	order := toposort2(prereqs)
+	for key, val := range order {
+		fmt.Printf("map[%d]=%s\n", key, val)
 	}
 }
 
