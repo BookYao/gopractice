18a73cc62d4462d6e940f8c25279b67cdcd54fde
BookYao <workfile_BookYao@163.com>
2020-08-23 23:59:45 +0800

    1. add celsiusFlag

---
+++ ch7/celsiusFlag.go
@@ -0,0 +1,57 @@
+/**
+ * @Author: BookYao
+ * @Description:
+ * @File:  celsiusFlag
+ * @Version: 1.0.0
+ * @Date: 2020/8/23 23:41
+ */
+
+package main
+
+import "fmt"
+
+type celsius float64
+
+type celsiusFlag struct  {
+	celsius
+}
+
+/*type Value interface {
+	 String() string
+	 Set(string) error
+}*/
+
+func Celsius(c celsius) celsius {
+	return (c*9/5+32)
+}
+
+func FToC(f celsius) celsius {
+	return (f-32)*5/9
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var value celsius
+	var unit string
+
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C":
+	f.celsius = Celsius(value)
+	return nil
+	case "F":
+	f.celsius = FToC(celsius(value))
+	return nil
+	}
+	return fmt.Errorf("invalid temperature: %q", s)
+}
+
+func main() {
+	var temp celsiusFlag
+	err := temp.Set("4C")
+	if err== nil {
+		fmt.Println(temp.celsius)
+	}
+
+}
+
+  
