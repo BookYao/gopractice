f9d7fc7ae4efc1851d2899af51590c5b6a762d13
BookYao <workfile_BookYao@163.com>
2020-09-08 18:30:17 +0800

    [add]  获取某个路径下所有文件

---
+++ ch8/thumbnail.go
@@ -0,0 +1,69 @@
+/**
+ * @Author: BookYao
+ * @Description:
+  提取某个路径下的图片的缩略图
+ * @File:  thumbnail
+ * @Version: 1.0.0
+ * @Date: 2020/9/8 16:31
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func getAllPic(dirname string) (pic []string, err error){
+	log.Printf("dirName:%s\n", dirname)
+	if len(dirname) == 0 {
+		log.Printf("Input dirname.\n")
+		return nil, fmt.Errorf("Input DirName.")
+	}
+
+	f, err := os.OpenFile(dirname, os.O_RDONLY, os.ModeDir)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	//参数：n,表读取目录的成员个数。通常传-1,表读取目录所有文件对象
+	fileinfo, _ := f.Readdir(-1)
+
+	//获取系统的路径分隔符
+	separator := string(os.PathSeparator)
+
+	for  _, info := range fileinfo {
+		if info.IsDir() {
+			childDir := dirname + separator + info.Name()
+			pic, err = getAllPic(childDir)
+			if err != nil {
+				log.Printf("Get ChildName Pic Failed. chileName:%s", childDir)
+				continue
+			}
+		} else {
+			//fmt.Println("infoName:", info.Name())
+			pic = append(pic, info.Name())
+		}
+	}
+
+	//fmt.Printf("====== %s DirFile Get Finish...====\n", dirname)
+	return pic, nil
+}
+
+func thumbnail() {
+	return
+}
+
+func main() {
+	dirname := "."
+	allPic, err := getAllPic(dirname)
+	if err != nil {
+		log.Printf("getAllPic failed. dirName:%s\n", dirname)
+	}
+	log.Println(allPic)
+	thumbnail()
+}
+
+  
