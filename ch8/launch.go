595c90d2d882e8920b034a0901a10728de11fd6d
BookYao <workfile_BookYao@163.com>
2020-09-18 00:14:45 +0800

    [update] 倒计时发射，但是回车可以打断

--- ch8/launch.go
+++ ch8/launch.go
@@ -1,6 +1,7 @@
 /**
  * @Author: BookYao
  * @Description:
+ update: 倒计时发射，但是回车可以打断
  * @File:  launch
  * @Version: 1.0.0
  * @Date: 2020/9/17 23:36
@@ -19,8 +20,8 @@ func launch() {
 }
 
 func main() {
-	/*tick := time.Tick(1 * time.Second)
-	for i := 10; i > 0; i-- {
+	tick := time.Tick(1 * time.Second)
+	/*for i := 10; i > 0; i-- {
 		fmt.Println(i)
 		<-tick
 	}*/
@@ -31,11 +32,16 @@ func main() {
 		abort <- struct{}{}
 	}()
 
-	select {
-	case <-time.After(10 * time.Second):
-	case <-abort:
-		fmt.Println("launch abort...")
-		return
+	for countDown := 10 ; countDown > 0; countDown -- {
+		fmt.Println(countDown)
+		select {
+		//case <-time.After(10 * time.Second):
+		case <- tick:
+		case <- abort:
+			fmt.Println("launch abort...")
+			return
+		}
 	}
+
 	launch()
 }
